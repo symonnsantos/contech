438fd028355d4e115cc47cded98713ce4746a745
symonnsantos <symonn.santos@gmail.com>
2021-02-17 19:18:34 -0300

    criação do usuarioService

---
+++ src/main/java/com/symonn/contech/resource/UsuarioResource.java
@@ -0,0 +1,65 @@
+package com.symonn.contech.resource;
+
+import com.symonn.contech.model.Pessoa;
+import com.symonn.contech.model.Usuario;
+import com.symonn.contech.repository.UsuarioRepository;
+import com.symonn.contech.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioResource {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @PostMapping
+    public ResponseEntity<Usuario> criar(@Valid @RequestBody Usuario usuario, HttpServletResponse response){
+    Usuario usuarioSalvo = usuarioRepository.save(usuario);
+    return ResponseEntity.status(HttpStatus.CREATED).body(usuarioSalvo);
+    }
+
+    @GetMapping
+    public List<Usuario> buscaTodos(){
+        return usuarioService.buscarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarPeloCodigo(@PathVariable Long id){
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        return usuario != null ? ResponseEntity.ok(usuario.get()) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Long id){
+        usuarioRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizar(@PathVariable Long id, @Valid @RequestBody Usuario usuario){
+        Usuario usuarioSalvo = usuarioService.atualizar(id, usuario);
+        return ResponseEntity.ok(usuarioSalvo);
+    }
+
+    @PutMapping("/{id}/ativo")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void setarAtivo(@PathVariable Long id, @RequestBody Boolean ativo){
+        usuarioService.setarAtivo(id, ativo);
+    }
+}
---
+++ src/main/java/com/symonn/contech/service/UsuarioService.java
@@ -0,0 +1,49 @@
+package com.symonn.contech.service;
+
+import com.symonn.contech.model.Usuario;
+import com.symonn.contech.repository.UsuarioRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> buscarTodos(){
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> buscaPeloId(Long id){
+        Optional<Usuario> usuarioSalvo = usuarioRepository.findById(id);
+        if(usuarioSalvo == null){
+            throw new EmptyResultDataAccessException(1);
+        }
+        return usuarioSalvo;
+    }
+
+    public Usuario atualizar(Long id, Usuario usuario){
+        Optional<Usuario> usuarioSalvo = usuarioRepository.findById(id);
+
+        BeanUtils.copyProperties(usuario, usuarioSalvo, "id");
+        return usuarioRepository.save(usuarioSalvo.get());
+    }
+
+    public Usuario setarAtivo(Long id, Boolean ativo){
+        Optional<Usuario> usuarioSalvo = usuarioRepository.findById(id);
+
+        usuarioSalvo.get().setAtivo(ativo);
+        return usuarioRepository.save(usuarioSalvo.get());
+    }
+
+    public void excluir(Long id){
+        Optional<Usuario> usuarioSalvo = usuarioRepository.findById(id);
+        usuarioRepository.deleteById(usuarioSalvo.get().getId());
+    }
+}
